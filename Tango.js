47d202475e264bed39ca1db072461f818dca7cac
Derek Elkins <derek.a.elkins@gmail.com>
2015-01-20 18:28:32 -0500

    Massively simplify transactions.

--- Tango.js
+++ Tango.js
@@ -36,6 +36,7 @@ function TangoRuntime(logStore, stream) {
     var offsets = {};
     var latestSeenOffset = 0;
     var objectRegistry = {};
+    var objectVersions = {};
 
     var persistentNameMap;
 
@@ -67,9 +68,7 @@ function TangoRuntime(logStore, stream) {
     };
 
     var make = function(typeName, name, oid, args) {
-        var factory = typeRegistry[typeName];
         args.unshift(oid);
-        var object = factory.apply(null, args);
         return logStore.append(stream, {
             type: INIT_ENTRY_TYPE,
             typeName: typeName,
@@ -93,7 +92,7 @@ function TangoRuntime(logStore, stream) {
 
     var internalFetch = function(typeName, name, oid, args) {
         return self.queryHelper(oid)
-                .then(function(obj) {
+                .then(function() {
                     var obj = objectRegistry[oid];
                     if(obj !== void(0)) {
                         return obj;
@@ -128,123 +127,52 @@ function TangoRuntime(logStore, stream) {
     //     Promise<int> checkpointHelper(int oid, any state);
     // }
 
-    var checkCommitEntry = function(entry, offset) {
-        var readSet = entry.readSet, writeSet = entry.writeSet;
-        if(readSet.length === 0) { // Write-only transactions can't fail.
-            return Q(true);
-        } else {
-            return checkConflicts(readSet, writeSet, Math.min(readSet[0], writeSet[0]), offset-1);
+    // Returns true if there are no conflicts.
+    var checkConflicts = function(readSet) {
+        // TODO: XXX
+        // If as we update objects (in the objectRegistry) we also maintain something that represents
+        // the current version.  Then we can ask the object if the current version is newer than and conflicts
+        // with the version referenced in the readSet which we need to store with the readSet.  This allows
+        // us to abort/commit a transaction without looking at the log at all.  This is probably what the Tango
+        // paper meant.  The run-time can maintain the offset to the most recently seen checkpoint and handle
+        // checkpoint conflicts without consulting the object (i.e. abort if mostRecentCheckpointOffset > readSet[i]).
+
+        var len = readSet.length;
+        for(var i = 0; i < len; ++i) {
+            var readVersion = readSet[i];
+            var oid = readVersion.oid;
+            var currentVersion = objectVersions[oid];
+            if(readVersion.offset < currentVersion.mostRecentCheckpointOffset) return false;
+            var obj = objectRegistry[oid];
+            if(obj.versionsConflict(currentVersion.customVersion, readVersion.version)) return false;
         }
-    };
-    
-    // Walk the writeSet of a potentially conflicting commit.
-    var checkWriteSetConflicts = function(reads, innerEntry, innerOffset) {
-        // TODO: XXX Need to be able to skip aborted transactions somehow.
-        // Preferably without having to recursively check them.
-        return checkCommitEntry(innerEntry, innerOffset)
-                .then(function(committed) {
-                    if(committed) {
-                        var writeSet = innerEntry.writeSet;
-                        return Q.all(writeSet.map(function(offset) { return logStore.read(stream, offset); }))
-                                .then(function(entries) {
-                                    var len = entries.length;
-                                    var succeeded = true, entry;
-                                    for(var i = 0; i < len; ++i) {
-                                        entry = entries[i];
-                                        if(entry.type === COMMIT_ENTRY_TYPE || entry.type === DECISION_ENTRY_TYPE)
-                                            throw 'TangoRuntime.checkWriteSetConflicts: Bad write set: ' + JSON.stringify(writeSet);
-                                        if(reads[entry.oid] !== void(0)) {
-                                            succeeded = false;
-                                            break;
-                                        }
-                                    }
-                                    return succeeded;
-                                });
-                    } else { 
-                        return true;
-                    }
-                });
+        return true;
     };
 
-    // Walk the log seeing if any writes conflict with the reads in readSet in the interval.
-    // Pre-conditions:
-    //  - readSet and writeSet are in order of ascending offsets
-    //  - Offsets in readSet or writeSet are always INIT/UPDATE/CHECKPOINT, NEVER COMMIT/DECISION.
-    //  - readSet and writeSet are disjoint.
-    //
-    // TODO: XXX For now, any writes to an OID (outside of the transaction) conflict.
-    var checkConflicts = function(readSet, writeSet, startIndex, stopIndex) {
-        var reads = {}, readIx = 0, writeIx = 0;
-        var aborted = false;
-        var sidetracks = [];
-        var deferred = Q.defer();
-        var observer = logStore.traverse(stream, startIndex, stopIndex);
-        observer.onNext = function(entry, offset) {
-            if(offset === readSet[readIx]) {
-                if(entry.type === COMMIT_ENTRY_TYPE || entry.type === DECISION_ENTRY_TYPE)
-                    throw 'TangoRuntime.checkConflicts: Bad read set: ' + JSON.stringify(readSet) + ' at offset ' + offset;
-                reads[entry.oid] = entry;
-                readIx++;
-                return true;
-            } else if(offset === writeSet[writeIx]) {
-                if(entry.type === COMMIT_ENTRY_TYPE || entry.type === DECISION_ENTRY_TYPE)
-                    throw 'TangoRuntime.checkConflicts: Bad write set: ' + JSON.stringify(writeSet) + ' at offset ' + offset;
-                writeIx++;
-                return true;
-            } else {
-                if(entry.type === COMMIT_ENTRY_TYPE && !entry.deleted) {
-                    sidetracks.push(checkWriteSetConflicts(reads, entry, offset));
-                    return true;
-                } else if(entry.type === DECISION_ENTRY_TYPE && !entry.deleted) { // TODO
-                    throw 'TangoRuntime.checkConflicts: DECISION_ENTRY_TYPE not implemented.';
-                } else if(reads[entry.oid] !== void(0) && !entry.speculative && !entry.deleted) { // conflict
-                    // TODO: This needs to be more fine-grained, which also means using an
-                    // object indexed only by oid is inadequate.
-                    aborted = true;
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        };
-        observer.onError = function(err) { deferred.reject(err); };
-        observer.onCompleted = function() { 
-            if(aborted) {
-                deferred.resolve(false);
-            } else {
-                Q.all(sidetracks).then(function(didSucceed) {
-                    var len = didSucceed.length;
-                    for(var i = 0; i < len; ++i) {
-                        if(!didSucceed[i]) {
-                            deferred.resolve(false);
-                        }
-                    }
-                    deferred.resolve(true);
-                }).done();
-            }
-        };
-        return deferred.promise;
-    };
-
-    var applyInit = function(entry, oid) {
+    var applyInit = function(entry, oid, offset) {
         var factory = typeRegistry[entry.typeName];
         if(objectRegistry[oid] !== void(0)) throw 'Tango.Runtime.applyInit: Duplicate init entries.';
         if(factory === void(0)) throw 'Tango.Runtime.applyInit: Unrecognized type: ' + entry.typeName;
-        objectRegistry[oid] = factory.apply(null, entry.state);
+        var obj = factory.apply(null, entry.state);
+        objectRegistry[oid] = obj;
+        objectVersions[oid] = { mostRecentCheckpointOffset: offset, customVersion: obj.initializeVersion(offset) };
     };
 
     var applyWrite = function(entry, offset) {
         var oid = entry.oid;
         var entryType = entry.type;
         if(entryType === INIT_ENTRY_TYPE) {
-            applyInit(entry, oid);
+            applyInit(entry, oid, offset);
         } else {
             var obj = objectRegistry[oid];
             if(obj === void(0)) throw 'Tango.Runtime.applyWrite: Unregistered object for OID: ' + oid;
+            var currentVersion = objectVersions[oid];
             if(entryType === UPDATE_ENTRY_TYPE) {
                 obj.applyUpdate(entry.value, offset); 
+                currentVersion.customVersion = obj.updateVersion(currentVersion.customVersion, entry.value, offset);
             } else if(entryType === CHECKPOINT_ENTRY_TYPE) {
                 obj.applyCheckpoint(entry.state, offset); 
+                currentVersion.mostRecentCheckpointOffset = offset;
             } else {
                 throw 'Tango.Runtime.applyWrite: Unexpected entry type, ' + entryType + ', at offset ' + offset;
             }
@@ -252,16 +180,13 @@ function TangoRuntime(logStore, stream) {
         advanceTo(oid, offset);
     };
 
-    var applyWrites = function(writeOffsets) {
+    var applyWrites = function(writeOffsets, offset) {
         return Q.all(writeOffsets.map(function(offset) { 
-                    return logStore.read(stream, offset)
-                            .then(function(entry) {
-                                return { entry: entry, offset: offset };
-                            });
+                    return logStore.read(stream, offset);
                 }))
                 .then(function(writeEntries) {
-                    writeEntries.forEach(function(e) {
-                        applyWrite(e.entry, e.offset);
+                    writeEntries.forEach(function(entry) {
+                        applyWrite(entry, offset);
                     });
                 });
     };
@@ -270,11 +195,11 @@ function TangoRuntime(logStore, stream) {
         if(latestSeenOffset >= offset) return; // TODO: Or should this be an error?
         latestSeenOffset = offset;
         if(entry.deleted || entry.speculative) return; // Ignore these records.
-        var oid, obj;
+        var oid, obj, currentVersion;
         switch(entry.type) {
             case INIT_ENTRY_TYPE:
                 oid = entry.oid;
-                applyInit(entry, oid);
+                applyInit(entry, oid, offset);
                 advanceTo(oid, offset);
                 break;
             case CHECKPOINT_ENTRY_TYPE:
@@ -282,6 +207,8 @@ function TangoRuntime(logStore, stream) {
                 obj = objectRegistry[oid];
                 if(obj === void(0)) throw 'Tango.Runtime.handleEntry: Unregistered object for OID: ' + oid;
                 obj.applyCheckpoint(entry.state, offset);
+                currentVersion = objectVersions[oid];
+                currentVersion.mostRecentCheckpointOffset = offset;
                 advanceTo(oid, offset);
                 break;
             case UPDATE_ENTRY_TYPE:
@@ -289,22 +216,16 @@ function TangoRuntime(logStore, stream) {
                 obj = objectRegistry[oid];
                 if(obj === void(0)) throw 'Tango.Runtime.handleEntry: Unregistered object for OID: ' + oid;
                 obj.applyUpdate(entry.value, offset); 
+                currentVersion = objectVersions[oid];
+                currentVersion.customVersion = obj.updateVersion(currentVersion.customVersion, entry.value, offset);
                 advanceTo(oid, offset);
                 break;
             case COMMIT_ENTRY_TYPE:
                 var readSet = entry.readSet, writeSet = entry.writeSet;
-                if(readSet.length === 0) { // Write-only transactions can't fail.
-                    return applyWrites(writeSet);
-                } else {
-                    return checkConflicts(readSet, writeSet, Math.min(readSet[0], writeSet[0]), offset-1)
-                            .then(function(succeeded) {
-                                if(succeeded) {
-                                    return applyWrites(writeSet);
-                                } else {
-                                    return; // Commit was aborted, so we just ignore it.
-                                }
-                            });
-                }
+                if(readSet.length === 0 || checkConflicts(readSet)) {
+                    return applyWrites(writeSet, offset);
+                } // else commit failed, so ignore it.
+                break;
             case DECISION_ENTRY_TYPE:
                 throw 'Tango.Runtime.handleEntry: Decision entry type not implemented.';
                 break;
@@ -365,7 +286,7 @@ function TangoRuntime(logStore, stream) {
             oid: oid,
             value: value,
             deleted: false,
-            speculative: false
+            speculative: speculative
         });
     };
 
@@ -402,21 +323,19 @@ function TangoRuntime(logStore, stream) {
 
     // TODO: This is an unbuffered implementation.  It would be nice to have the buffer size be configurable
     // from 0 (this) to unbounded.
-    var TangoTransaction = function(offsets) {
+    var TangoTransaction = function() {
         var self = this;
         var writeSet = [], readSet = [], completedMsg = '';
         var readHash = {}, writeHash = {};
 
         self.queryHelper = function(oid/*, stopIndex*/) {
             if(completedMsg !== '') return Q.reject(completedMsg);
-            // TODO: XXX Don't include reads that occur after writes conflicting writes.
-            // E.g. register.set(T, f(register.get(T))); register.get(T);  The second get should NOT
-            // record a read.  The second get should also see the value of the set, so this needs to notify
+            // TODO: XXX 
+            // E.g. register.set(T, f(register.get(T))); register.get(T);
+            // The second get should see the value of the set, so this needs to notify
             // a parallel copy of the object to apply the update represented by the write.
-            // 
-            // And, um, don't push duplicate reads ...
             if(!readHash[oid] && !writeHash[oid]) {
-                readSet.push(offsets[oid]);
+                readSet.push({ oid: oid, offset: offsets[oid], version: objectVersions[oid].customVersion });
                 readHash[oid] = true;
             }
             return Q();
@@ -454,12 +373,10 @@ function TangoRuntime(logStore, stream) {
                     writeSet: writeSet,
                     deleted: false
                 }).then(function(offset) {
-                    return checkConflicts(readSet, writeSet, Math.min(readSet[0], writeSet[0]), offset-1)
-                        .then(function(succeeded) {
-                            completedMsg = succeeded ? 'Transaction already committed.'
-                                                     : 'Transaction aborted.';
-                            return succeeded ? Q() : Q.reject('Transaction aborted.');
-                        });
+                    var succeeded = checkConflicts(readSet);
+                    completedMsg = succeeded ? 'Transaction already committed.'
+                                             : 'Transaction aborted.';
+                    return succeeded ? Q() : Q.reject('Transaction aborted.');
                 });
             } else if(writeSet.length > 0) { // Write-only transaction
                 completedMsg = 'Transaction already committed.';
@@ -472,12 +389,10 @@ function TangoRuntime(logStore, stream) {
                     return; // Write-only transactions can't fail.
                 });
             } else if(readSet.length > 0) { // Read-only transaction
-                return checkConflicts(readSet, writeSet, readSet[0], readSet[readSet.length-1])
-                        .then(function(succeeded) {
-                            completedMsg = succeeded ? 'Transaction already committed.'
-                                                     : 'Transaction aborted.';
-                            return succeeded ? Q() : Q.reject('Transaction aborted');
-                        });
+                var succeeded = checkConflicts(readSet);
+                completedMsg = succeeded ? 'Transaction already committed.'
+                                         : 'Transaction aborted.';
+                return succeeded ? Q() : Q.reject('Transaction aborted');
             } else { // Empty transaction
                 completedMsg = 'Transaction already committed.';
                 return Q();
@@ -523,11 +438,7 @@ function TangoRuntime(logStore, stream) {
      */
     self.beginTransaction = function() {
         return  self.queryHelper(0).then(function() {
-            var offsetsSnapshot = {};
-            Object.keys(offsets).forEach(function(oid) {
-                offsetsSnapshot[oid] = offsets[oid];
-            });
-            return new TangoTransaction(offsetsSnapshot);
+            return new TangoTransaction();
         });
     };
 
@@ -557,6 +468,9 @@ TangoRuntime.init = function(logStore, stream) {
 // interface ITangoObject {
 //     void applyUpdate(any value, int offset);
 //     void applyCheckpoint(any state, int offset);
+//     bool versionsConflict(any currentVersion, any oldVersion);
+//     any initializeVersion(int offset);
+//     any updateVersion(any currentVersion, any update, int offset);
 // }
 
 /**
@@ -573,6 +487,15 @@ function TangoRegister(oid, initialValue) {
     };
     self.applyCheckpoint = function(state) {
         box = state;
+    };
+    self.versionsConflict = function(currentVersion, oldVersion) {
+        return false; // If the checkpoint check didn't fail, then the versions are the same.
+    };
+    self.initializeVersion = function(offset) {
+        return undefined;
+    };
+    self.updateVersion = function(currentVersion, update, offset) {
+        return undefined;
     };
 
     /**
@@ -616,6 +539,15 @@ function TangoCounter(oid, initialValue) {
     };
     self.applyCheckpoint = function(state) {
         box = state;
+    };
+    self.versionsConflict = function(currentVersion, oldVersion) {
+        return currentVersion !== oldVersion;
+    };
+    self.initializeVersion = function(offset) {
+        return offset;
+    };
+    self.updateVersion = function(currentVersion, update, offset) {
+        return offset;
     };
 
     /**
@@ -696,6 +628,15 @@ function TangoQueue(oid) {
             }
         }
     };
+    self.versionsConflict = function(currentVersion, oldVersion) { // TODO: XXX Improve me.
+        return currentVersion !== oldVersion;
+    };
+    self.initializeVersion = function(offset) {
+        return offset;
+    };
+    self.updateVersion = function(currentVersion, update, offset) {
+        return offset;
+    };
 
     /**
      * @method enqueue
@@ -765,6 +706,15 @@ function TangoMap(oid, initialMapping) {
     };
     self.applyCheckpoint = function(state) {
         map = state;
+    };
+    self.versionsConflict = function(currentVersion, oldVersion) { // TODO: XXX Improve me.
+        return currentVersion !== oldVersion;
+    };
+    self.initializeVersion = function(offset) {
+        return offset;
+    };
+    self.updateVersion = function(currentVersion, update, offset) {
+        return offset;
     };
 
     /**
